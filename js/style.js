d4b16e3fd36b972e5db70db194ffc81d25f5249c
kth616 <nitraid.0106@gmail.com>
2022-01-20 22:35:02 +0900

    Google フォームの非同期実装

--- js/style.js
+++ js/style.js
@@ -29,3 +29,28 @@ jQuery('a[href^="#"]').click(function() {
   );
   return false;
 });
+
+$(document).ready(function () {
+
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScUAon6hoJyQlymYAtt5dKn_sL7cJKeSeeeaDsICBnQwKlnwA/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".end-message").slideDown();
+          $(".submit-btn").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
